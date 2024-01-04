e8ef77e03858fb909766a557e65d6634f189dddc
umer ghori <umerfayyaz500@gmail.com>
2024-01-04 12:32:14 +0500

    small change

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -86,26 +86,27 @@
 <div class="row">
 <div class="col-md-3">
 <div class="count_content">
-<h3>$ <span class="counter">305</span> </h3>
+<h3>{{config('app.currency')}} <span class="counter">305</span> </h3>
 <p>Today's Income</p>
 </div>
 </div>
+
 <div class="col-md-3">
 <div class="count_content">
-<h3>$ <span class="counter">1005</span> </h3>
-<p>This Week's Income</p>
-</div>
-</div>
-<div class="col-md-3">
-<div class="count_content">
-<h3>$ <span class="counter">5505</span> </h3>
+<h3>{{config('app.currency')}} <span class="counter">5505</span> </h3>
 <p>This Month's Income</p>
 </div>
 </div>
 <div class="col-md-3">
 <div class="count_content">
-<h3>$ <span class="counter">155615</span> </h3>
+<h3>{{config('app.currency')}} <span class="counter">155615</span> </h3>
 <p>This Year's Income</p>
+</div>
+</div>
+<div class="col-md-3">
+<div class="count_content">
+<h3>{{config('app.currency')}} <span class="counter">1005</span> </h3>
+<p>Total Income</p>
 </div>
 </div>
 </div>
