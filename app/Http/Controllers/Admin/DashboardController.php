320fac6f5291fccdc0f56971de17a2a75860c9c5
umer ghori <umerfayyaz500@gmail.com>
2024-03-05 21:03:29 +0500

    income according to time

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\Models\{
     SubCategory,
     Category,
     ContactUs,
     Review,
     Order,
-    Subscriber
+    Subscriber,
+    Product
 };
 
 class DashboardController extends Controller
@@ -26,7 +28,18 @@ class DashboardController extends Controller
         $count_contactus = ContactUs::count();
         $count_reviews = Review::count();
         $count_orders = Order::where('status',0)->count();
-        return view('backend.home',compact('count_category','count_sub_category','count_contactus','count_reviews','count_orders'));
+        $product_count = Product::count();
+
+        /*income*/
+        $today_income  = Order::whereDate('created_at', Carbon::today())->sum('grand_total');
+        $monthly_income  = Order::whereBetween('created_at', 
+                                [
+                                    Carbon::now()->startOfMonth(), 
+                                    Carbon::now()->endOfMonth()
+                                ])->sum('grand_total');
+        $total_income  = Order::sum('grand_total');
+        /*end*/
+        return view('backend.home',compact('count_category','count_sub_category','count_contactus','count_reviews','count_orders','today_income','total_income','monthly_income','product_count'));
     }
     public function subscribers()
     {
--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -32,7 +32,7 @@
 {{-- <img src="{{asset('dashboard/img/icon/wheel.svg')}}" alt=""> --}}
 </div>
 <div class="count_content">
-<h3><span class="counter">7509</span> </h3>
+<h3><span class="counter">{{$product_count}}</span> </h3>
 <p>Products</p>
 </div>
 </div>
@@ -86,32 +86,27 @@
 <div class="row">
 <div class="col-md-3">
 <div class="count_content">
-<h3>{{config('app.currency')}} <span class="counter">305</span> </h3>
+<h3>{{config('app.currency')}} <span class="counter">{{$today_income}}</span> </h3>
 <p>Today's Income</p>
 </div>
 </div>
 
 <div class="col-md-3">
 <div class="count_content">
-<h3>{{config('app.currency')}} <span class="counter">5505</span> </h3>
+<h3>{{config('app.currency')}} <span class="counter">{{$monthly_income}}</span> </h3>
 <p>This Month's Income</p>
 </div>
 </div>
+
 <div class="col-md-3">
 <div class="count_content">
-<h3>{{config('app.currency')}} <span class="counter">155615</span> </h3>
-<p>This Year's Income</p>
-</div>
-</div>
-<div class="col-md-3">
-<div class="count_content">
-<h3>{{config('app.currency')}} <span class="counter">1005</span> </h3>
+<h3>{{config('app.currency')}} <span class="counter">{{$total_income}}</span> </h3>
 <p>Total Income</p>
 </div>
 </div>
 </div>
 </div>
-<div id="bar_wev"></div>
+
 </div>
 </div>
 
